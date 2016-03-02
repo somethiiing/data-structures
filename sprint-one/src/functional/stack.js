84b7de542246c2dfc4a4d3c14c6fa6b7f6ee4679
somethiiing <wilsonweichengyu@gmail.com>
2016-03-02 14:36:56 -0800

    Report the size when size function is called

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,20 @@
 var Stack = function() {
   var someInstance = {};
+  var size = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+
   };
 
   someInstance.pop = function() {
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
