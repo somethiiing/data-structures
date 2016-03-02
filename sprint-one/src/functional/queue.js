6026c44c5b894d9cf73ac882b543b4ff19b04fc4
somethiiing <wilsonweichengyu@gmail.com>
2016-03-02 15:01:05 -0800

    Report a size of zero for a new queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,6 @@
 var Queue = function() {
   var someInstance = {};
+  var size = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -13,6 +14,7 @@ var Queue = function() {
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
