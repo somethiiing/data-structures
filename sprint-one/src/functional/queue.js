ca597597aea1bf095d6529ced05812975dd5c865
somethiiing <wilsonweichengyu@gmail.com>
2016-03-02 15:02:39 -0800

    Increments when enqueue is called

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -8,6 +8,8 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[size] = value;
+    size++;
   };
 
   someInstance.dequeue = function() {
