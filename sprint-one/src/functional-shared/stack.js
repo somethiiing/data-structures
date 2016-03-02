63cab47904d03b2deea76a10ff6b79430f9dff8c
somethiiing <wilsonweichengyu@gmail.com>
2016-03-02 15:47:47 -0800

    A new stack will report size 0 after size function called

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,19 @@
-var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+var Stack = function() {
+  var storage = {};
+  storage.index = 0;
+
+  _.extend(storage, stackMethods);
+
+  return storage;
 };
 
-var stackMethods = {};
+var stackMethods = {
 
+  size: function() {
+    return this.index;
+  }
+
+};
 
