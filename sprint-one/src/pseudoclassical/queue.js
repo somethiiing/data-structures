7dfbb0de8e2e012a0cc25e59b274f25162897958
somethiiing <wilsonweichengyu@gmail.com>
2016-03-03 10:23:28 -0800

    Implement queue data structure using pseudoclassical instantiation

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,31 @@
-var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+var Queue = function() {
+  this.index = 0;
 };
+
+Queue.prototype.size = function() {
+  return this.index;
+};
+
+Queue.prototype.enqueue = function(value) {
+  this[this.index] = value;
+  this.index++;
+};
+
+Queue.prototype.dequeue = function() {
+  var temp = this[0];
+  if (this.index > 0) {
+    for (var key in this) {
+      if (Number(key) >= 0) {
+        this[key] = this[Number(key) + 1];
+      }
+    }
+    this.index--;
+  }
+  return temp;
+};
+
+var queue = new Queue();
 
 
