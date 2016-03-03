17667a61d3df3024e03c6c8ef7def6428a9d7b5f
somethiiing <wilsonweichengyu@gmail.com>
2016-03-02 17:18:03 -0800

    Cause dequeue to check if the key is of type num before iterating

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -18,11 +18,13 @@ var queueMethods = {
     this[this.index] = value;
     this.index++;
   },
-  
+
   dequeue: function() {
     var temp = this[0];
     for (var key in this) {
-      this[key] = this[Number(key) + 1];
+      if (Number(key)) {
+        this[key] = this[Number(key) + 1];
+      }
     }
     this.index--;
     return temp;
