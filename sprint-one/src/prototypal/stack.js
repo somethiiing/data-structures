86acc22dccb51ac34123eaa61a0a3d8ca34936ae
somethiiing <wilsonweichengyu@gmail.com>
2016-03-03 10:04:24 -0800

    Properly implement a stack data structure using prototypal instantiation

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,27 @@
-var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+var Stack = function() {
+  var instance = Object.create(stackMethods);
+  instance.storage = {};
+  instance.index = 0;
+
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size: function() {
+    return this.index;
+  },
+  push: function(value) {
+    this[this.index] = value;
+    this.index++;
+  },
+  pop: function() {
+    if (this.index > 0) {
+      this.index--;
+      return this[this.index];
+    }
+  }
+};
 
 
