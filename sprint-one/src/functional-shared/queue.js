bd964edd244e8a80218b3dfbff7271bbe746de04
somethiiing <wilsonweichengyu@gmail.com>
2016-03-02 16:20:14 -0800

    Size function returns size

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,18 @@
-var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+var Queue = function() {
+  var storage = {};
+  storage.index = 0;
+
+  _.extend(storage, queueMethods);
+
+  return storage;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function() {
+    return this.index;
+  }
+};
 
 
