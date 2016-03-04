c128aa75f44209ebe233cf22344e112bcc989bda
somethiiing <wilsonweichengyu@gmail.com>
2016-03-04 15:40:30 -0800

    Properly implement set with methods add, contains and remove

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,31 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  for (var key in this._storage) {
+    if (this._storage[key] === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ add - O(1)
+ contains - O(n)
+ remove - O(1)
  */
