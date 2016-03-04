f05bb407329752de2d4b143d423db80666ec97cd
somethiiing <wilsonweichengyu@gmail.com>
2016-03-03 16:41:35 -0800

    add the addChild function

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,8 +2,10 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree, treeMethods);
+
+  newTree.children = [];
+
 
   return newTree;
 };
@@ -11,9 +13,44 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var newChild = Tree(value);
+  this.children.push(newChild);
 };
 
 treeMethods.contains = function(target) {
+  // for (var key in this) {
+  //   for (var i = 0; i < this[this.key].length; i++) {
+  //     if (this[key][i] === target) {
+  //       return true;
+  //     }
+  //   }
+  // }
+  // return false;
+
+  for (var i = 0; i < this[this.key].length; i++) {
+    for (var key in this[i][key]) {
+      if (this[i][key] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
+  // for (var i = 0; i < this.children.length; i++) {
+  //   if (this.children[i].value === target) {
+  //     return true;
+  //   }
+  //   return treeMethods.contains(target);
+  // }
+  // return false;
+  // var initialize = false;
+  // for (var i = 0; i < ...; i++) {
+  //   if (this.children[i].value === target) {
+  //     initialize = true;
+  //   }
+  // }
+
+  // return initialize;
+
 };
 
 
@@ -21,3 +58,5 @@ treeMethods.contains = function(target) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// var tree = Tree();
