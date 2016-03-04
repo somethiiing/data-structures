6711f3c5e87f3bd32948c37b63614d0993d7dbda
somethiiing <wilsonweichengyu@gmail.com>
2016-03-03 19:50:11 -0800

    Add complexity notation for the functions

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -39,6 +39,7 @@ treeMethods.contains = function(target) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
- */
+  addChild - O(n)
+  contains - O(n)
 
-// var tree = Tree();
+ */
