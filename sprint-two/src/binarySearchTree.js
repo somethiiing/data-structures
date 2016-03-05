48eafc839143598e40bb8b95411838ac9de4e5b8
somethiiing <wilsonweichengyu@gmail.com>
2016-03-04 22:20:28 -0800

    Allow for additions to the tree by value

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,48 @@
 var BinarySearchTree = function(value) {
+  var binarySearchTree = createNode(value, binarySearchTreeMethods);
+
+  return binarySearchTree;
+};
+
+var binarySearchTreeMethods = {
+  
+  insert: function(value) {
+    var newNode = createNode(value, binarySearchTreeMethods);
+
+    var placeNode = function(node) {
+      if (this.value > node.value) {
+        if (this.left === null) {
+          this.left = newNode;
+        } else {
+          placeNode.call(this.left, node);
+        }
+      } else if (this.value < node.value) {
+        if (this.right === null) {
+          this.right = newNode;
+        } else {
+          placeNode.call(this.right, node);
+        }
+      }
+    };
+
+    placeNode.call(this, newNode);
+
+  },
+  contains: function(value) {
+    //return a boolean
+    
+  },
+  depthFirstLog: function(cb) {
+    //maps callback to each value in tree
+  },
+  left: null,
+  right: null
+};
+
+var createNode = function(value, inheritsFrom) {
+  var instance = Object.create(inheritsFrom);
+  instance.value = value;
+  return instance;
 };
 
 
