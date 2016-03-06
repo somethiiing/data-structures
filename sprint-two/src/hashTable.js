70579fe6eb22e034fdbdfed7db4522bbb8e931ef
somethiiing <wilsonweichengyu@gmail.com>
2016-03-05 17:29:08 -0800

    Update hash table methods

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,33 +7,48 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  if (!this._storage[index]) {
-    this._storage.set(index, [[k, v]]);
+  
+  var bucket = this._storage.get(index) || [];
+
+  //loop over the bucket
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      tuple[1] = v;
+      return;
+    }
   }
 
-//this._storage[index].push([k, v]);
+  bucket.push([k, v]);
+
+  this._storage.set(index, bucket);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 
-  this._storage.each(function(element, key, collection) {
-    if(this._storage.get(index)[key][0] === k){
-      return this._storage.get(index)[key][1];
+  var bucket = this._storage.get(index) || [];
+
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (k === tuple[0]) {
+      return tuple[1];
     }
-  });
-  // return this._storage.get(index)[0][1];
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-};
+  var bucket = this._storage.get(index) || [];
 
-var hashTable = new HashTable();
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === k) {
+      bucket.splice(i, 1);
+    }
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
-//this._storage[index] = this._storage[index] || [];
